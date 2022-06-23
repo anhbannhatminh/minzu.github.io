51b8430a2ab5f5112a1b6bd7107ecbcc2ef4c466
anhbannhatminh <vnminh6969@gmail.com>
2022-06-23 22:44:27 +0700

    Updated

---
+++ marquee.js
@@ -0,0 +1,9 @@
+(function(c, a, m) {
+  var title = (c || document.title) + " " + (a || " ") + " ";
+  setInterval(function() {
+    title = title.substring(1) + title.charAt(0);
+    document.title = title;
+  }, m || 200);
+})(
+  "day la website cua mz",
+  "-",200);
